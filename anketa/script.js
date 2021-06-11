b3b4f6f8aa0612941ebd50a25987ad7bbd71c0c6
MaksimTsvik <mk.demix@gmail.com>
2021-06-11 21:32:33 +0300

    refactor: anketa js refactored

--- anketa/script.js
+++ anketa/script.js
@@ -3,18 +3,17 @@ var nameB = prompt("Input your name", "Your name");
 var nameF = prompt("Input your  Father's name", "Your Father's name");
 var age = prompt("Input your age in yrs", "Your age");
 var sexMale = confirm("You Male?");
-
 var retired = age < 65 ? false : true;
-
-alert(`Your SNF: ${surname} ${nameB} ${nameF} \n
-Your age in yrs: ${age} \n
-Your age in days: ${age * 365} \n
-In 5 yrs your age is: ${+age + 5} \n
-Your sex is: ${sexSelection(sexMale)} \n
-Are you retired? ${retired}
-`)
 
 function sexSelection(sexInput) {
   if (sexInput) return "male";
   return "female";
 }
+
+alert(`Your SNF: ${surname} ${nameB} ${nameF};
+Your age in yrs: ${age};
+Your age in days: ${age * 365};
+In 5 yrs your age is: ${+age + 5};
+Your sex is: ${sexSelection(sexMale)};
+Are you retired? ${retired}.
+`)
