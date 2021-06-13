3b75ee1517d1cb5339e1f63ee49309300b53f61e
MaksimTsvik <mk.demix@gmail.com>
2021-06-14 00:01:01 +0300

    start: added a3+ task

---
+++ A3/script.js
@@ -0,0 +1,14 @@
+const string = prompt("Input a string");
+
+function isPalindrome(str) {
+  const replacer = /\W/g;
+  str = str.replace(replacer, "").toLowerCase();
+  console.log(str);
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) return false;
+  }
+  return true
+}
+
+isPalindrome(string) ? alert("HOORAY!\n It is a PALINDROME") :
+  alert("OH NOOOO!\n It is not a PALINDROME");
