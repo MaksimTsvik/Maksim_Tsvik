2f37f35c4d60dc850d12a07be1bc5b2f4569da98
MaksimTsvik <mk.demix@gmail.com>
2021-06-14 00:43:59 +0300

    feat: deleted unnecessary logs from A2+ task

--- A2/script.js
+++ A2/script.js
@@ -3,9 +3,8 @@ let string = prompt("Input a string");
 while (string.startsWith(" ")) {
   string = string.slice(1);
 }
-console.log(string);
 while (string.endsWith(" ")) {
   string = string.slice(0, -1);
 }
-console.log(string);
+
 alert(`%${string}%`);
