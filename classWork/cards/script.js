87fb6bdcfef119a377d5910701ed747d9fc31f97
MaksimTsvik <mk.demix@gmail.com>
2021-06-21 22:42:55 +0300

    feat: updated classWork folder

--- classWork/cards/script.js
+++ classWork/cards/script.js
@@ -14,7 +14,7 @@ const phones = [
     imgUrl: "./assets/iphone12pro.jpeg",
     color: "blue",
     descriptions: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam consequuntur quis velit aut facere exercitationem vel reprehenderit ab quibusdam possimus. Enim soluta qui itaque saepe, maiores doloremque aperiam dolorem a?",
-    price: 100,
+    price: 200,
     sale: true
   },
   {
@@ -22,18 +22,18 @@ const phones = [
     imgUrl: "./assets/m11ultra.jpeg",
     color: "white",
     descriptions: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam consequuntur quis velit aut facere exercitationem vel reprehenderit ab quibusdam possimus. Enim soluta qui itaque saepe, maiores doloremque aperiam dolorem a?",
-    price: 100,
+    price: 300,
     sale: false
   }
 ];
 
 for (phone of phones) {
-
   newItemHtml += `<div class="card" style="width: 18rem;">
   <img src="${phone.imgUrl}" class="card-img-top" alt="${phone.model}">
   <div class="card-body">
     <h5 class="card-title">${phone.model}</h5>
     <p class="card-text">${phone.descriptions}</p>
+    <p class="card-text">COLOR: ${phone.color}</p>
     <p class="card-text">PRICE: ${phone.price}</p>
     <a href="#" class="btn btn-primary">BUY NOW</a>
     <a href="#" class="btn btn-primary" style = "${phone.sale ? "" : "display:none"};">ON SALE</a>
