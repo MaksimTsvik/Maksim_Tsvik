497227be5f45a0ca8f6f64f229b2b782c160d93e
MaksimTsvik <mk.demix@gmail.com>
2021-06-22 18:06:58 +0300

    init:start b4 task

--- b3/script.js
+++ b3/script.js
@@ -1,1 +1,10 @@
 const string = prompt("Input a mathematical expression", "2*(-3+1)");
+
+function calculate(str) {
+
+  return +str;
+}
+
+// alert((new Function("return " + string)())); -IT WORKS!
+
+alert(calculate(string));
---
+++ b4/script.js
@@ -0,0 +1,5 @@
+"use strict"
+
+function buldWrapper(tag) {
+  
+}
