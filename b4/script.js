41ea8e8ea2192ba311c71adbd0e1eacaa046851d
MaksimTsvik <mk.demix@gmail.com>
2021-07-04 00:23:40 +0300

    feat: b4+ task completed

--- b4/script.js
+++ b4/script.js
@@ -1,5 +1,34 @@
 "use strict"
 
-function buldWrapper(tag) {
-  
+function buildWrapper(tag) {
+  return function (text) {
+    const textArr = text.split('');
+    textArr.forEach(function (letter, index) {
+      switch (letter) {
+        case ('<'):
+          textArr[index] = '&lt;';
+          break;
+        case ('>'):
+          textArr[index] = '&gt;';
+          break;
+        case ("'"):
+          textArr[index] = '&apos;';
+          break;
+        case ('"'):
+          textArr[index] = '&quot;';
+          break;
+        case ('&'):
+          textArr[index] = '&amp;';
+          break;
+      }
+    })
+    const adjText = textArr.join('');
+    return (`<${tag}>${adjText}</${tag}>`)
+  }
 }
+
+const wrapH1 = buildWrapper("H1");
+const wrapP = buildWrapper("P");
+console.log(wrapH1("СТИХИ"));
+console.log(wrapP("Однажды в студёную зимнюю пору"));
+console.log(wrapP("Вкусные M&M's"));
