30e1856aad1f84393b2c26b822f1737d7985742a
MaksimTsvik <mk.demix@gmail.com>
2021-07-09 21:25:29 +0300

    feat: updated classwork

--- classWork/formClass/script.js
+++ classWork/formClass/script.js
@@ -1,6 +1,5 @@
 const app = document.querySelector(".app");
 let phones = [];
-let id = 0;
 
 function pushItem() {
   const imgUrl = document.getElementById("iUrl");
@@ -8,10 +7,8 @@ function pushItem() {
   const model = document.getElementById("pModel");
   const price = document.getElementById("pPrice");
   const sale = document.getElementById("onSale");
-
-  phones.push({ "imgUrl": imgUrl.value, "brand": brand.value, "model": model.value, "price": price.value, "sale": sale.checked, "id": id })
-
-  id++;
+  phones.push({ "imgUrl": imgUrl.value, "brand": brand.value, "model": model.value, "price": price.value, "sale": sale.checked })
+  console.log(phones);
 }
 
 function clearScr() {
@@ -28,8 +25,6 @@ function render() {
       <p class="card-text">PRICE: ${phone.price}</p>
       <a href="#" class="btn btn-primary">BUY NOW</a>
       <a href="#" class="btn btn-primary" style = "${phone.sale ? "" : "display:none"};">ON SALE</a>
-      <hr>
-      <button type="button" class="btn btn-secondary btn-lg" data-id="${phone.id}">DELETE</button>
     </div>
   </div> `;
   }
@@ -39,15 +34,3 @@ function render() {
 document.getElementById("addBtn").addEventListener("click", pushItem);
 document.getElementById("clearScrBtn").addEventListener("click", clearScr);
 document.getElementById("renderBtn").addEventListener("click", render);
-
-window.addEventListener("click", function (e) {
-  let index = e.target.dataset.id;
-  if (index !== undefined) {
-    // phones = phones.filter(item => item.id != index);
-    console.log(index)
-    phones.splice(index, 1);
-    console.log(phones)
-    render();
-  }
-});
-
