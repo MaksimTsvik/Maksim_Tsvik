72dd0099b4e32d7c2d37a7ec3800372bf74cb46b
MaksimTsvik <mk.demix@gmail.com>
2021-08-03 17:49:51 +0300

    feat: update basket drop in class task

--- classWork/formClass/script.js
+++ classWork/formClass/script.js
@@ -1,6 +1,9 @@
 const app = document.querySelector(".app");
-let phones = [];
-let id = 0;
+let phones = [{ "imgUrl": "https://content2.onliner.by/catalog/device/header@2/751e57c3ea68eb504fed4f16066f77e7.jpeg", "brand": 'Samsung', "model": 'S21', "price": '2100', "sale": true, "id": '0' },
+{ "imgUrl": "https://content2.onliner.by/catalog/device/header@2/e2189f90f9088975c553ec33431fc186.jpeg", "brand": 'Apple', "model": "11 64GB", "price": '1670', "sale": true, "id": '1' },
+{ "imgUrl": "https://content2.onliner.by/catalog/device/header@2/7d15facb87374d6519bc391445db281c.jpeg", "brand": 'Xiaomi', "model": "Redmi Note 10S", "price": '639', "sale": false, "id": '2' },
+];
+let id = 3;
 
 class MobilePhone {
 
@@ -24,12 +27,12 @@ class MobilePhone {
     let newItemHtml = "";
     for (let phone of phones) {
       newItemHtml += `<div class="card" style="width: 18rem;" data-id="${phone.id}" draggable="true">
-      <img src="${phone.imgUrl}" class="card-img-top" alt="${phone.brand}">
+      <img src="${phone.imgUrl}" class="card-img-top" alt="${phone.brand}" draggable="false">
       <div class="card-body">
         <h5 class="card-title">${phone.model}</h5>
         <p class="card-text">PRICE: ${phone.price}</p>
-        <a href="#" class="btn btn-primary">BUY NOW</a>
-        <a href="#" class="btn btn-primary" style = "${phone.sale ? "" : "display:none"};">ON SALE</a>
+        <a href="#" class="btn btn-primary" draggable="false">BUY NOW</a>
+        <a href="#" class="btn btn-primary" draggable="false" style = "${phone.sale ? "" : "display:none"};">ON SALE</a>
         <hr>
         <button type="button" class="btn btn-secondary btn-lg" data-id="${phone.id}">DELETE</button>
       </div>
@@ -41,7 +44,7 @@ class MobilePhone {
 }
 
 let mobilePhone = new MobilePhone();
-
+mobilePhone.render();
 
 document.getElementById("addBtn").addEventListener("click", mobilePhone.pushItem);
 document.getElementById("clearScrBtn").addEventListener("click", mobilePhone.clearScr);
@@ -55,12 +58,17 @@ window.addEventListener("click", function (e) {  //delete an item via DELETE but
   }
 });
 
-const basket = document.querySelector(".basket"); //find basket - place where to add drop listener
+const basket = document.querySelector(".basket"); //find basket (shopping cart) - place where to add drop listener
 const totalPrice = document.querySelector(".total_price"); //
 const itemsCount = document.querySelector('.basket_nmb')
+
+const trash = document.querySelector('.trash');
+const trashCount = document.querySelector('.trash_nmb');
+
 let itemId = null;
 let priceCount = 0;
 let itemsCounter = 0;
+let trashCounter = 0;
 
 document.addEventListener("dragstart", function (ev) {
   itemId = ev.target.dataset.id;
@@ -71,14 +79,30 @@ document.addEventListener("dragover", function (ev) {
 });
 
 basket.addEventListener("drop", function (ev) {
-  itemsCounter++;
-  itemsCount.innerHTML = itemsCounter;
-
   for (let phone of phones) {
     if (phone.id == itemId) {
+      itemsCounter++;
+      itemsCount.innerHTML = itemsCounter;
       priceCount += +phone.price;
+      totalPrice.innerHTML = priceCount + " $";
+
+      const itemBlock = document.createElement("div");
+      const title = document.createElement("p");
+      title.innerHTML = phone.brand + " " + phone.model + " - " + phone.price + "$";
+      itemBlock.appendChild(title);
+      this.appendChild(itemBlock);
     }
   }
+});
 
-  totalPrice.innerHTML = priceCount + " $";
+trash.addEventListener("drop", function (ev) {
+  for (let phone of phones) {
+    if (phone.id == itemId) {
+      trashCounter++;
+      trashCount.innerHTML = trashCounter;
+
+      phones = phones.filter(item => item.id != phone.id);
+      mobilePhone.render();
+    }
+  }
 });
