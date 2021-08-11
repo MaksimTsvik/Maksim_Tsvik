0f5e3940203aa755f28139ae37d121effe25e1fe
MaksimTsvik <mk.demix@gmail.com>
2021-08-11 20:11:07 +0300

    feat: clock dom finished

--- clock_dom/script.js
+++ clock_dom/script.js
@@ -1,16 +1,31 @@
 //get clock container
 const clock = document.querySelector('.clock')
-//get clock board
-const clockCircle = document.querySelector('.clock_circle');
 
 const radius = 200; //setup radius where all numbers located
 let angleStep = 30; //setup step between numbers in degrees
 const numberSize = 40; //setup size of a numbers of clock in px
+const size = 500; //setup main clock size in px
+
+// create clock circle
+const clockCircle = document.createElement('div');
+clockCircle.style.cssText = `
+width: ${size}px;
+height: ${size}px;
+border-radius: 50%;
+background-color: rgba(104, 141, 198, 1);
+position: absolute;
+top: 50%;
+left: 50%; 
+transform: translate(-50%, -50%);
+`;
+clock.append(clockCircle);
 
 //find x and y coordinates of clock's center
 const clockCircleX = clockCircle.offsetLeft;
 const clockCircleY = clockCircle.offsetTop;
+console.log(clockCircle.offsetWidth)
 
+//add time scale at clock
 for (let i = 1; i < 13; i++) {
   //create a new div with time scale
   let number = document.createElement('div');
@@ -36,34 +51,80 @@ for (let i = 1; i < 13; i++) {
   clock.append(number);
   angleStep = angleStep + 30;
 }
+
 //create an arrows
 const secArrow = document.createElement('div');
 const minArrow = document.createElement('div');
 const hourArrow = document.createElement('div');
 //setup each arrow styling
-secArrow.style.cssText = `position: absolute; width: ${radius - numberSize}; height: 6px; background-color: red; left: ${clockCircleX};top: ${clockCircleY - 3}; transform-origin: 0%; transform: rotate(-90deg); transition: all 0.1s cubic-bezier(0.37, 3.18, 0.45, 1.15);`;
-minArrow.style.cssText = `position: absolute; width: ${radius - 1.5 * numberSize}; height: 8px; background-color: black; left: ${clockCircleX};top: ${clockCircleY - 4}; transform-origin: 0%; transform: rotate(-90deg); transition: all 0.1s cubic-bezier(0.37, 3.18, 0.45, 1.15);`;
-hourArrow.style.cssText = `position: absolute; width: ${radius - 2.5 * numberSize}; height: 10px; background-color: black; left: ${clockCircleX};top: ${clockCircleY - 5}; transform-origin: 0%; transform: rotate(-90deg); transition: all 0.1s cubic-bezier(0.37, 3.18, 0.45, 1.15);`;
+secArrow.style.cssText = `position: absolute; z-index:1; width: ${radius - numberSize}px; height: 6px; background-color: red; left: ${clockCircleX}px;top: ${clockCircleY - 3}px; transform-origin: 0%; transform: rotate(-90deg); transition: all 0.1s cubic-bezier(0.37, 3.18, 0.45, 1.15); opacity: 70%`;
+minArrow.style.cssText = `position: absolute; z-index:1; width: ${radius - 1.5 * numberSize}px; height: 8px; background-color: black; left: ${clockCircleX}px;top: ${clockCircleY - 4}px; transform-origin: 0%; transform: rotate(-90deg); transition: all 0.1s cubic-bezier(0.37, 3.18, 0.45, 1.15); opacity: 70%`;
+hourArrow.style.cssText = `position: absolute; z-index:1; width: ${radius - 2.5 * numberSize}px; height: 10px; background-color: black; left: ${clockCircleX}px;top: ${clockCircleY - 5}px; transform-origin: 0%; transform: rotate(-90deg); transition: all 0.1s cubic-bezier(0.37, 3.18, 0.45, 1.15); opacity: 70%`;
 //add arrows into the clock
 clock.append(secArrow);
 clock.append(minArrow);
 clock.append(hourArrow);
 
+//create a text time representation
+const digitalClock = document.createElement('div');
+clock.append(digitalClock);
+digitalClock.style.cssText = `position: absolute; text-align:center; 
+  width: ${clockCircle.offsetWidth / 2}px; 
+  height: ${numberSize}px;
+  left: ${clockCircleX}px;
+  top: ${clockCircleY}px;
+  line-height: ${numberSize}px;
+  transform: translate(-50%, -${3 * numberSize}px);
+  font-size: ${numberSize}px`;
+
 //setup a timer to obtain actual date
 const timer = setInterval(setDate, 1000);
-
+//use timer function to move arrows and update time
 function setDate() {
   const date = new Date();
   const seconds = date.getSeconds();
   const minutes = date.getMinutes();
   const hours = date.getHours();
+  //calculate arrows turn angle
   const secTransform = seconds * 6 - 90;
   const minutesTransform = minutes * 6 - 90;
   const hoursTransform = hours * 30 - 90;
+  //move an arrows
   secArrow.style.transform = `rotate(${secTransform}deg)`;
   minArrow.style.transform = `rotate(${minutesTransform}deg)`;
   hourArrow.style.transform = `rotate(${hoursTransform}deg)`;
+  //remove threshold at 12(24)/0 point
+  if (seconds == 0) {
+    secArrow.style.transition = 'none';
+  } else {
+    secArrow.style.transition = 'all 0.1s cubic-bezier(0.37, 3.18, 0.45, 1.15)';
+  }
+  if (minutes == 0) {
+    minArrow.style.transition = 'none';
+  } else {
+    minArrow.style.transition = 'all 0.1s cubic-bezier(0.37, 3.18, 0.45, 1.15)';
+  }
+  if (hours == 12 || hours == 24) {
+    hourArrow.style.transition = 'none';
+  } else {
+    hourArrow.style.transition = 'all 0.1s cubic-bezier(0.37, 3.18, 0.45, 1.15)';
+  }
+  //update digital clock data
+  digitalClock.innerHTML = `${formatDateTime(date)}`;
+}
 
-  
+// date formatter function (чч:мм:сс)
+function formatDateTime(dt) {
+  let hours = dt.getHours();
+  let minutes = dt.getMinutes();
+  let seconds = dt.getSeconds();
+  return str0l(hours, 2) + ':' + str0l(minutes, 2) + ':' + str0l(seconds, 2);
+}
 
+// add 0 to val in dependance of Len
+function str0l(val, len) {
+  let strVal = val.toString();
+  while (strVal.length < len)
+    strVal = '0' + strVal;
+  return strVal;
 }
